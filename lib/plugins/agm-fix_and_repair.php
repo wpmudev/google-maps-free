67b2943e09090d27f760f8f00e1021b649b9a31d
vladislav <ve@incsub.com>
2017-09-12 07:13:32 +0200

    Fix add-on authorship

--- lib/plugins/agm-fix_and_repair.php
+++ lib/plugins/agm-fix_and_repair.php
@@ -4,5 +4,5 @@ Plugin Name: Fix and Repair
 Description: Tools for repairing your Google Maps installation.
 Plugin URI: http://premium.wpmudev.org/project/wordpress-google-maps-plugin
 Version: 1.0
-Author: Ve Bailovity (Incsub)
+Author: WPMU DEV
 */
--- lib/plugins/agm-kml_uploader.php
+++ lib/plugins/agm-kml_uploader.php
@@ -4,5 +4,5 @@ Plugin Name: KML Uploader
 Description: Allows you to upload your own KML files. <b>Please, make sure you activate the KML Overlay plugin first.</b>
 Plugin URI: http://premium.wpmudev.org/project/wordpress-google-maps-plugin
 Version: 1.0
-Author: Ve Bailovity (Incsub)
+Author: WPMU DEV
 */
